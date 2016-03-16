e3560efa08bc3eafabb37f89b941b2fb8a79afba
Ben McClure <bmcclure@topfloortech.com>
2016-03-16 14:01:15 -0500

    Fix class names

--- src/Mailer.php
+++ src/Mailer.php
@@ -18,8 +18,8 @@ class Mailer {
     protected static $initialized = false;
 
     public static $protocols = [
-        'mandrill' => '\\TopFloor\\Utility\\MailProtocols\\MandrillMailHandler',
-        'smtp' => '\\TopFloor\\Utility\\MailProtocols\\SmtpMailHandler',
+        'mandrill' => '\\TopFloor\\Utility\\MailHandlers\\MandrillMailHandler',
+        'smtp' => '\\TopFloor\\Utility\\MailHandlers\\SmtpMailHandler',
     ];
 
     public static function initialize() {
